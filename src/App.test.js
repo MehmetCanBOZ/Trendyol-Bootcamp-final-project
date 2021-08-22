09801cc45b75ed5f7873bd6e38b5bffcd524eb83
MehmetCanBOZ <mehmet.boz@agu.edu.tr>
2021-08-22 17:43:24 +0300

    test configuration setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {shallow} from 'enzyme';
+import App from "./App";
+import { findByTestAttr } from "./test/testUtils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const setup = () => shallow(<App/>);
+
+describe("CardHolder Component Test",() =>{
+  let wrapper;
+
+  beforeEach(()=>{
+    wrapper = setup();
+  });
+
+  
+  test('renders header-wrapper', () => {
+    const appHeader= findByTestAttr(wrapper,'app-header');
+
+    expect(appHeader.length).toBe(1);
+  });
+  test('renders header-wrapper', () => {
+    const appHeaderText = findByTestAttr(wrapper,'app-header').text();
+
+    expect(appHeaderText).toBe("Reversed Spider Solitaire");
+  });
+   
 });
