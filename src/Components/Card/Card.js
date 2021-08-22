68014e8ca7071e5195f707ff1aa4dd158a45970a
MehmetCanBOZ <mehmet.boz@agu.edu.tr>
2021-08-22 20:39:42 +0300

    sass initials added

---
+++ src/Components/Card/Card.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from 'prop-types';
+import Suit from "../../assets/images/spade.png"
+import "./Card.scss";
+import { cardState } from "../../logic/Helper/Helper";
+
+const Card = ({ card }) => {
+  const [down, setdown] = useState("");
+  
+  useEffect(() => {
+    setdown(cardState(card));
+  }, [card.isDown]);
+  
+  return (
+    <div data-test="card-wrapper" className={"card__wrapper"  + down }>
+      <div data-test="card-content-left" className="card__content card__rank-left">{ card.rank }</div>
+      <div data-test="card-suit" className="card__content card__suit"><img src={Suit} width="70px" height="70px" draggable="false" /></div>
+      <div data-test="card-content-right" className="card__content card__rank-right">{ card.rank }</div>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  card:PropTypes.object.isRequired
+}
+
+export default Card;
