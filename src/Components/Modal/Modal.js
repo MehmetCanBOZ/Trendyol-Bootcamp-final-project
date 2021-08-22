918b1d24ddc2456d7b7ad180d8f9470c6121a90f
MehmetCanBOZ <mehmet.boz@agu.edu.tr>
2021-08-22 21:41:01 +0300

    Modal component test,styles added

---
+++ src/Components/Modal/Modal.js
@@ -0,0 +1,24 @@
+import "./Modal.scss";
+import { resetGameState } from "../../logic/Helper/Helper";
+
+function Modal( {setGame}) {
+  return (
+    <div data-test="modal-container" className="container">
+      <div data-test="modal-wrapper" className = "modal-wrapper">
+        <div data-test="modal-header" className="modal-header">
+          <p data-test="modal-header-info">CONGRATULATIONS</p>
+        </div>
+        <div data-test="modal-content" className="modal-content">
+          <div data-test="modal-body" className="modal-body">
+            <p data-test="modal-info">You complete 8 hands and won the game. If you want to play again, click the play again. Enjoyyyyyy !!!</p>
+          </div>
+          <div data-test="modal-footer" className="modal-footer">
+            <button data-test="modal-button"  className="btn-cancel" onClick={(e) => resetGameState(setGame)}>Play Again</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
---
+++ src/Components/Modal/Modal.test.js
@@ -0,0 +1,60 @@
+import {shallow} from 'enzyme';
+import Modal from "./Modal";
+import { findByTestAttr } from "../../test/testUtils";
+
+const setup = () => shallow(<Modal/>);
+
+describe("CardHolder Component Test",() =>{
+  let wrapper;
+
+  beforeEach(()=>{
+    wrapper = setup();
+  });
+
+  describe("render element without error",()=>{
+    test('renders modal-container', () => {
+      const modalContainer = findByTestAttr(wrapper,'modal-container');
+
+      expect(modalContainer.length).toBe(1);
+    });
+
+    test('renders modal-wrapper', () => {
+      const modalWrapper = findByTestAttr(wrapper,'modal-wrapper');
+
+      expect(modalWrapper.length).toBe(1);
+    });
+
+    test('renders modal-header', () => {
+      const modalHeader= findByTestAttr(wrapper,'modal-header');
+
+      expect(modalHeader.length).toBe(1);
+    });
+
+    test('renders modal-header-info', () => {
+      const modalHeaderInfo= findByTestAttr(wrapper,'modal-header-info');
+      const modalHeaderInfoText= findByTestAttr(wrapper,'modal-header-info').text();
+
+      expect(modalHeaderInfo.length).toBe(1);
+      expect(modalHeaderInfoText).toBe("CONGRATULATIONS");
+    });
+
+    test('renders modal-content-info', () => {
+      const modalContent= findByTestAttr(wrapper,'modal-content');
+        
+      expect(modalContent.length).toBe(1);
+    });
+
+    test('renders modal-button', () => {
+      const modalButton = findByTestAttr(wrapper,'modal-button');
+    
+      expect(modalButton.length).toBe(1);
+    });
+    test('renders modal-info', () => {
+      const modalInfo= findByTestAttr(wrapper,'modal-info');
+      const modalInfoText= findByTestAttr(wrapper,'modal-info').text();
+        
+      expect(modalInfo.length).toBe(1);
+      expect(modalInfoText).toBe("You complete 8 hands and won the game. If you want to play again, click the play again. Enjoyyyyyy !!!");
+    }); 
+  });
+});
