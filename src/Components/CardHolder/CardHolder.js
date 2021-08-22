05e92d7fa3d20bd6048f41c59d0e0f1d5c0d39a4
MehmetCanBOZ <mehmet.boz@agu.edu.tr>
2021-08-22 21:10:09 +0300

    CardHolder component test,styles added

---
+++ src/Components/CardHolder/CardHolder.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CardHolder.scss";
+
+function CardHolder() {
+  return <div data-test="card-holder" className="cardholder"></div>;
+}
+
+export default CardHolder;
---
+++ src/Components/CardHolder/CardHolder.test.js
@@ -0,0 +1,21 @@
+import {shallow} from 'enzyme';
+import CardHolder from "./CardHolder";
+import { findByTestAttr } from "../../test/testUtils";
+
+const setup = () => shallow(<CardHolder/>);
+
+describe("CardHolder Component Test",() =>{
+  let wrapper;
+
+  beforeEach(()=>{
+    wrapper = setup();
+  });
+
+  describe("render element without error",()=>{
+    test('renders card-holder without error', () => {
+      const cardHolder = findByTestAttr(wrapper,'card-holder');
+
+      expect((cardHolder).exists()).toBeTruthy();
+    });
+  }); 
+});
